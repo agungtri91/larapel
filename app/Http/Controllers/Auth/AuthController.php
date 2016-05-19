5da9734ebf974c4dfb7e70a3c9721070f63bb6cb
agung6706140114 <agungtri91@gmail.com>
2016-05-19 15:43:41 +0700

    commit pertama

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -51,6 +51,7 @@ class AuthController extends Controller
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
+            'alamat' => 'required|max:255',
             'password' => 'required|min:6|confirmed',
         ]);
     }
@@ -66,6 +67,7 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'alamat' => $data['alamat'],
             'password' => bcrypt($data['password']),
         ]);
     }
